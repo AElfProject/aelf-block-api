05895eb37a6af1a68fc761d495ae3781f3ba386a
hzz780 <hzz780@qq.com>
2019-03-13 15:56:19 +0800

    ...诶，老脸通红，UNION和解决的事情，搞了这么多’

--- app/core/baseService.js
+++ app/core/baseService.js
@@ -17,69 +17,52 @@ class BaseService extends Service {
     // 2. select count(*) as xx from xxx;
     async selectQuery(pool, sql, sqlValues) {
         // TODO: move to config / redis / db
-        const rollbackCount = 60;
-        const unConfirmedList = ['transactions', 'blocks', 'resources'];
+
+        // select count(*)
+        // select count(*) from transactions_unconfirmed where address_to='3AhZRe8RvTiZUBdcqCsv37K46bMU2L2hH81JF8jKAnAUup9'
+        //  and params_to='3L8EHf4CMq2CHr9ifrmhNYf5hDH1DET1LNZKwzhJr8hFa2H'
+        // UNION select count(*) from transactions_0 where address_to='3AhZRe8RvTiZUBdcqCsv37K46bMU2L2hH81JF8jKAnAUup9'
+        //  and params_to='3L8EHf4CMq2CHr9ifrmhNYf5hDH1DET1LNZKwzhJr8hFa2H';
+
+        // pagination
+        // select * from transactions_unconfirmed where address_to = '3AhZRe8RvTiZUBdcqCsv37K46bMU2L2hH81JF8jKAnAUup9'
+        // and params_to = '3L8EHf4CMq2CHr9ifrmhNYf5hDH1DET1LNZKwzhJr8hFa2H'
+        // UNION select * from transactions_0 where address_to = '3AhZRe8RvTiZUBdcqCsv37K46bMU2L2hH81JF8jKAnAUup9'
+        // and params_to = '3L8EHf4CMq2CHr9ifrmhNYf5hDH1DET1LNZKwzhJr8hFa2H'
+        // order by block_height desc limit 10 offset 0;
+
+        const secondPartList = ['transactions', 'blocks', 'resources'];
+        const tableSuffix = '_unconfirmed';
         // demo: ['from  transactions_0 '], ['from  blocks_0 '], ['from resource_0 ']
         const matchUnconfirmedReg
-            = new RegExp(`from\\s*(${unConfirmedList.join('|')})_\\d*\\s*`, 'g');
+            = new RegExp(`from\\s*(${secondPartList.join('|')})_\\d*\\s*`, 'g');
         const table = sql.match(matchUnconfirmedReg); // ["from  transactions_0 "];
-        const tableUnconfirmed = table[0].replace(/_\d*/, '_unconfirmed');
-        let unconfirmedSql = sql.replace(table[0], tableUnconfirmed);
 
-        const blankList = sql.match(/\w*\s*\?\s*/g);
-        let limitIndex = -1;
-        let offsetIndex = -1;
-        const blankListLenght = blankList.length;
-        for (let i = 0; i < blankListLenght; i++) {
-            if (blankList[i].includes('limit')) {
-                limitIndex = i;
-            }
-            else if (blankList[i].includes('offset')) {
-                offsetIndex = i;
-            }
-        }
-
-        const offsetNum = sqlValues[offsetIndex]; // n
-        const limitNum = sqlValues[limitIndex]; // m
         // 需要处理unconfirmed的逻辑
         if (table && table.length) {
-            if (offsetNum > rollbackCount) {
-                return await pool.query(sql, sqlValues);
-            }
-            // offsetNum belongs to [0, rollbackCount]
-            if ((offsetNum + limitNum) <= rollbackCount) {
-                return await pool.query(unconfirmedSql, sqlValues);
-            }
+            const tableUnconfirmed = table[0].replace(/_\d*/, tableSuffix);
+            let unconfirmedSql = sql.replace(table[0], tableUnconfirmed);
+            const orderString = unconfirmedSql.toLocaleLowerCase().match(/order.*/g, '') || [''];
+            unconfirmedSql = unconfirmedSql.toLocaleLowerCase().replace(/order.*/g, '');
 
-            let sqlValuesUnconfirmed = Array.from(sqlValues);
-            sqlValuesUnconfirmed[limitIndex] = rollbackCount - offsetNum;
+            const paramsArray = orderString[0].match(/\?/g) || [];
+            const lengthRemove = paramsArray.length;
+            let valueTemp = Array.from(sqlValues);
+            valueTemp.length = valueTemp.length - lengthRemove;
 
-            let sqlValuesConfirmed = Array.from(sqlValues);
-
-            sqlValuesConfirmed[offsetIndex] = rollbackCount + 1;
-            sqlValuesConfirmed[limitIndex] = offsetNum + limitNum - rollbackCount;
-
-            // 如果是count的话，count相加。
-            // 如果是分页的话，合并两个数组。
-            if (sql.includes('limit') && sql.includes('offset')) {
-                const partUnconfirmed = await pool.query(unconfirmedSql, sqlValuesUnconfirmed);
-                const partConfirmed = await pool.query(sql, sqlValuesConfirmed);
-                return [...partUnconfirmed, ...partConfirmed];
-            }
-            // count的计算是不精准的
+            const finalSql = unconfirmedSql + ' UNION ' + sql;
+            const result = await pool.query(finalSql, [...valueTemp, ...sqlValues]);
             if (sql.includes('count(')) {
-                unconfirmedSql
-                    = unconfirmedSql.toLocaleLowerCase().replace(/count\(\*\)\s*as\s*total/, 'block_height')
-                    + ` order by block_height limit ${rollbackCount} offset 0`;
-                const partUnconfirmed = await pool.query(unconfirmedSql, sqlValuesUnconfirmed);
-                const countUnconfirmed = partUnconfirmed.length;
                 const sqlArray = sql.toLocaleLowerCase().split(/\s+/);
                 const asKey = sqlArray[sqlArray.indexOf('as') + 1];
 
-                let partConfirmed = await pool.query(sql, sqlValuesConfirmed);
-                partConfirmed[0][asKey] += countUnconfirmed;
-                return partConfirmed;
+                const output = result[0][asKey] + result[1][asKey];
+                return [{
+                    [asKey]: output
+                }];
             }
+
+            return result;
         }
         return await pool.query(sql, sqlValues);
     }
