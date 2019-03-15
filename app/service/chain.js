2625058cde2be8459baeef970a9a398bde207099
bikedawuwang <710101991@qq.com>
2019-03-15 11:32:20 +0800

    fix: remove  max(block_height)

--- app/service/chain.js
+++ app/service/chain.js
@@ -34,8 +34,8 @@ class ChainService extends BaseService {
             let getTxsSql = `select * from transactions_0  where chain_id=? ORDER BY block_height ${order} limit ? offset ? `;
             let getCountSql = `select count(*) as total from transactions_0  where chain_id=?`;
             // return sql;
-            let txs = await this.selectQuery(getTxsSql, [chain_id, limit, offset]);
-            let count = await this.selectQuery(getCountSql, [chain_id]);
+            let txs = await this.selectQuery(aelf0, getTxsSql, [chain_id, limit, offset]);
+            let count = await this.selectQuery(aelf0, getCountSql, [chain_id]);
             // let result = await aelf0.query('select * from blocks_0 ORDER BY block_height ASC limit 10 offset 0');
             return {
                 total: count[0].total,
