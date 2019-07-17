8741e78916c285f1e9ca95f65d848ae9329967df
hzz780 <hzz780@qq.com>
2019-07-17 19:23:49 +0800

    remove admin

--- app/middleware/isAdmin.js
+++ app/middleware/isAdmin.js
@@ -9,21 +9,21 @@ module.exports = options => {
     return async function isAdmin(ctx, next) {
         await next();
 
-        const aelf0 = ctx.app.mysql.get('aelf0');
+        // const aelf0 = ctx.app.mysql.get('aelf0');
 
-        const checkAdmin = 'select id from users where address=?';
-        const accountAddress = ctx.cookies.get('account', {
-            signed: false
-        });
+        // const checkAdmin = 'select id from users where address=?';
+        // const accountAddress = ctx.cookies.get('account', {
+        //     signed: false
+        // });
 
-        const isAdmin = await aelf0.query(checkAdmin, [accountAddress]);
+        // const isAdmin = await aelf0.query(checkAdmin, [accountAddress]);
 
-        if (isAdmin.length <= 0) {
-            ctx.body = JSON.stringify({
-                error: 200001,
-                errorMessage: 'no permission'
-            });
-            return;
-        }
+        // if (isAdmin.length <= 0) {
+        //     ctx.body = JSON.stringify({
+        //         error: 200001,
+        //         errorMessage: 'no permission'
+        //     });
+        //     return;
+        // }
     };
 };
