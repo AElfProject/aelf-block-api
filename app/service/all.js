9526f3f9cf4e25b57102eaa50ce17d23a074752e
potter-sun <potter.sun@aelf.io>
2023-02-10 20:51:47 +0800

    fix: 🐛 get block total

--- app/service/all.js
+++ app/service/all.js
@@ -105,10 +105,18 @@ class AllService extends BaseService {
     };
   }
 
+  async getLatestBlock() {
+    const aelf0 = this.ctx.app.mysql.get('aelf0');
+    const offset = 0;
+    const limit = 1;
+    const getBlocksSql =
+    'select (block_height) from blocks_0 order by block_height DESC limit ? offset ?';
+    const blocks = await this.selectQuery(aelf0, getBlocksSql, [ limit, offset ]);
+    return blocks.block_height;
+  }
+
   async getAllBlocksInner(options) {
-    const { redisKeys } = this.app.config;
-    let blocksCount = await this.redisCommand('get', redisKeys.blocksCount) || 0;
-    blocksCount = parseInt(blocksCount, 10);
+    const blocksCount = await this.getLatestBlock();
     const aelf0 = this.ctx.app.mysql.get('aelf0');
     const {
       limit, page, order, offset: _offset
